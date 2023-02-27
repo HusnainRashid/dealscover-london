037df2f6dc4f55929f1379e7eab63dfad202a036
Caroline-Amanquah <carolineamanquah@gmail.com>
2023-02-27 14:16:34 +0000

    attempted link instead of navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import './index.css';
 import Search from './components/Search/Search';
 import EventsView from './components/EventsView/EventsView';
 import IconsMenu from './components/IconsMenu/IconsMenu';
 import Navbar from './components/navbar/Navbar';
-// import LoginForm from '../auth/LoginForm';
 import SignUpForm from './components/user/SignUpForm';
 import LogInForm from './components/auth/LoginForm';
+
+
+
+// function App() {
+//   return (
+//     <html>
+//       <head>
+//         <title>Learn react</title>
+//       </head>
+//       <body>
+//         <div className="landing-page">
+//           <Navbar  />
+//           <h1>London on a Budget</h1>
+//           <Search />
+//           <IconsMenu />
+//           <EventsView />
+//           <SignUpForm />
+//           <LogInForm />
+          
+//         </div>
+//       </body>
+//     </html>
+//   );
+// }
+
 
 function App() {
   return (
@@ -15,20 +40,22 @@ function App() {
       <head>
         <title>Learn react</title>
       </head>
-      <body>
-        <div className="landing-page">
-          <h1>London on a Budget</h1>
-          <Search />
-          <IconsMenu />
-          <EventsView />
-          <SignUpForm />
-          <LogInForm />
-          
+       <body>
+        <div className="App">
+          <Routes>
+          <Route path="/" element={ < Search/> } />
+          <Route path="/" element={ < EventsView/> } />
+          <Route path="/" element={ < IconsMenu/> } />
+          <Route path="/" element={ < Navbar/> } />
+          <Route path="/login" element={ <LogInForm/> } />
+          <Route path="/signup" element={ <SignUpForm/> } />
+          </Routes>
         </div>
-      </body>
+        </body>
     </html>
   );
 }
 
 
 export default App;
+
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,26 +1,30 @@
 // navbar on homepage : login and sign up button 
 
-import React, { useState } from "react";
+import React from "react";
 import './Navbar.css';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
+    // const [searchTerm, setSearchTerm] = useState('');
+    // const [searchResults, setSearchResults] = useState([]);
 
-    const handleSignOut = () => {
-      window.localStorage.removeItem("token");
-      window.location.href = "/login";
-    };
+    // const handleSignOut = () => {
+    //   window.localStorage.removeItem("token");
+    //   window.location.href = "/login";
+    // };
       return (
       <div align="right">
           <a href="/login-page">
           <button type="submit">
             <i class="login">Login</i>
+            <Link to="login">Login</Link>
           </button>
         </a>
         <a href="/signup-page">
           <button type="submit">
             <i class="signup">Signup</i>
+            <Link to="signup">Signup</Link>
           </button>
         </a>
       </div>
@@ -29,3 +33,4 @@ const Navbar = () => {
 
 
 export default Navbar;
+
