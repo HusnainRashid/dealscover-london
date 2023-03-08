aca5b1f68150c284c021e25527b60bbcce888e42
Ehijie <eaghedo1@gmail.com>
2023-03-08 00:26:49 +0000

    part developed eventview component

--- client/src/components/EventView/EventView.js
+++ client/src/components/EventView/EventView.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 
-const EventView = ({ event }) => {
+const EventView = () => {
   const {id} = useParams()
+  const [event, setEvent] = useState([])
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+
+  useEffect(() => {
+    fetch(`/events/${id}`)
+    .then(response => response.json())
+    .then(async data => {
+      window.localStorage.setItem("token", data.token);
+      setToken(window.localStorage.getItem("token"));
+      setEvent(data.event);
+    })
+  }, [])
+
   return (
     <div>
-      <p>Event - {id}</p>
+      <p>{event._id}</p>
     </div>
   );
 };
---
+++ server/spec/controllers/events.spec.js
@@ -0,0 +1,18 @@
+// const app = require("../../app");
+// const request = require("supertest");
+// require("../mongodb_helper");
+// const User = require("../../models/event");
+
+// describe("/events", () = {
+//     beforeEach(async () => {
+//         await Event.deleteMany({});
+//     });
+
+//     describe("Find event by id", () => {
+//         test("find event by id", async () => {
+//             await (await request(app).post("/events")).send({
+
+//             })
+//         })
+//     })
+// })
