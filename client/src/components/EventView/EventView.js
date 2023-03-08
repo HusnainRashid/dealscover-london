a17a9b15ba74556e06d9c178bddf33c415ee875a
Ehijie <eaghedo1@gmail.com>
2023-03-08 01:51:37 +0000

    removed maximum price from eventview component

--- client/src/components/EventView/EventView.js
+++ client/src/components/EventView/EventView.js
@@ -21,12 +21,8 @@ const EventView = () => {
       <h3>Accessibility: {event.accessibility}</h3>
       <h3>Age restriction: {event.ageRestricted}</h3>
       <h3>Address: {event.venueAddress}, {event.postCode}</h3>
-      <h3>
-        Minimum Price: {event.priceRange[0].min === 0 ? "Free" : `£${event.priceRange[0].min}`}
-      </h3>
-      <h3>
-        Maximum Price: {event.priceRange[0].max === 0 ? "" : `£${event.priceRange[0].max}`} 
-      </h3>
+      {/* <h3>Minimum Price: {event.priceRange[0].min === 0 ? "Free" : `£${event.priceRange[0].min}`}</h3> */}
+   
       <a href={`https://${event.url}`} target='_blank' rel='noreferrer'><button className="btn btn-light">
                   Grab a ticket here!
                 </button></a>
